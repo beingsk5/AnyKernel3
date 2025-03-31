3a0b6b367aead6717eb8b5a47de866788b5adb88
beingsk5 <takeiteasy4possible@gmail.com>
2025-03-31 15:30:10 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,17 +4,24 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=RagnaR-Kernel by beingsk5
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
-device.name4=tuna
-device.name5=
+device.name1=OnePlus7
+device.name2=OnePlus7Pro
+device.name3=OnePlus7T
+device.name4=OnePlus7TPro
+device.name5=guacamoleb
+device.name6=guacamole
+device.name7=hotdogb
+device.name8=hotdog
+device.name9=OnePlus7TProNR
+device.name10=hotdogg
+device.name11=guacamoles
+device.name12=OnePlus7ProNR
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -29,8 +36,8 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
-IS_SLOT_DEVICE=0;
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
 
@@ -43,18 +50,6 @@ dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_b
 # init.rc
 backup_file init.rc;
 replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "bootscript" init.tuna;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
-patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
-patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
-append_file fstab.tuna "usbdisk" fstab;
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
