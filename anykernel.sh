4c898c84aa2d290db832330d60a2297941e29ae5
beingsk5 <takeiteasy4possible@gmail.com>
2025-04-21 18:34:50 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -5,23 +5,26 @@
 # global properties
 properties() { '
 kernel.string=RagnaR-Kernel by beingsk5
+kernel.made=beingsk5
+message.word=Thank you for installing RagnaR-Kernel !
 do.devicecheck=1
 do.modules=0
 do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=OnePlus7
-device.name2=OnePlus7Pro
-device.name3=OnePlus7T
-device.name4=OnePlus7TPro
-device.name5=guacamoleb
-device.name6=guacamole
-device.name7=hotdogb
-device.name8=hotdog
-device.name9=OnePlus7TProNR
-device.name10=hotdogg
-device.name11=guacamoles
-device.name12=OnePlus7ProNR
+device.name2=oneplus7
+device.name3=guacamoleb
+device.name4=OnePlus7Pro
+device.name5=guacamole
+device.name6=OnePlus7ProTMO
+device.name7=guacamolet
+device.name8=OnePlus7T
+device.name9=hotdogb
+device.name10=OnePlus7TPro
+device.name11=hotdog
+device.name12=OnePlus7TProNR
+device.name13=hotdogg
 supported.versions=
 supported.patchlevels=
 supported.vendorpatchlevels=
@@ -35,11 +38,11 @@ set_perm_recursive 0 0 755 644 $RAMDISK/*;
 set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
-# boot shell variables
-block=/dev/block/bootdevice/by-name/boot;
+# shell variables
+block=/dev/block/by-name/boot;
 is_slot_device=1;
-RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
@@ -48,21 +51,21 @@ PATCH_VBMETA_FLAG=auto;
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
 # init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
+# backup_file init.rc;
+# replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
 
-# init.tuna.rc
- backup_file init.tuna.rc;
- insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
- append_file init.tuna.rc "bootscript" init.tuna;
- 
- # fstab.tuna
- backup_file fstab.tuna;
- patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
- patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
- patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
- append_file fstab.tuna "usbdisk" fstab;
- 
+# # init.tuna.rc
+# backup_file init.tuna.rc;
+# insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
+# append_file init.tuna.rc "bootscript" init.tuna;
+
+# # fstab.tuna
+# backup_file fstab.tuna;
+# patch_fstab fstab.tuna /system ext4 options "noatime,barrier=1" "noatime,nodiratime,barrier=0";
+# patch_fstab fstab.tuna /cache ext4 options "barrier=1" "barrier=0,nomblk_io_submit";
+# patch_fstab fstab.tuna /data ext4 options "data=ordered" "nomblk_io_submit,data=writeback";
+# append_file fstab.tuna "usbdisk" fstab;
+
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ## end boot install
 
@@ -125,4 +128,3 @@ write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_
 
 #write_boot; # use flash_boot to skip ramdisk repack, e.g. for dtb on devices with hdr v4 but no vendor_kernel_boot
 ## end vendor_boot install
-
